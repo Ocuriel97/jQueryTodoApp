1384d8469e888a54f77cd56aa0d33f383c2e669c
Omar Curiel <omarcuriel@Omars-MacBook-Pro.local>
2018-05-05 11:26:55 -0700

    added complete function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-//check off by clicking
 $('li').click(function(){
-  if($(this).css('color') === 'rgb(128,128,128)'){
-    console.log('grey af');
-  } else {
-  $(this).css({
-    color: 'grey',
-    textDecoration: 'line-through'
-  });
-}
+  $(this).toggleClass('completed');
 });
