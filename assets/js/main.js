3f6b92c91ca1dcc5ef93db8b1d5d1b9b8555ffb4
Omar Curiel <omarcuriel@Omars-MacBook-Pro.local>
2018-05-11 08:43:34 -0700

    finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,10 @@ $('input[type="text"]').keypress(function(e){
   if(e.which === 13){
     var todoText = $(this).val();
     $(this).val('');
-    $('ul').append('<li><span> X </span>' + todoText + '</li>');
+    $('ul').append('<li><span><i class = "fa fa-trash"></i></span> ' + todoText + '</li>');
   }
 })
+
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
+});
