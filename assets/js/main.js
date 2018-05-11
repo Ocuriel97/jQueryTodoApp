fd8cab37ba4f5469e2ee34d1646d13489395847f
Omar Curiel <omarcuriel@Omars-MacBook-Pro.local>
2018-05-11 08:05:13 -0700

    all the logic is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
-$('li').click(function(){
+//check off
+$('ul').on('click','li',function(){
   $(this).toggleClass('completed');
 });
+
+//click X to delete
+$('ul').on('click','span',function(e){
+  $(this).parent().fadeOut(500,function(){
+    $(this).remove();
+  });
+  e.stopPropagation();
+})
+
+$('input[type="text"]').keypress(function(e){
+  if(e.which === 13){
+    var todoText = $(this).val();
+    $(this).val('');
+    $('ul').append('<li><span> X </span>' + todoText + '</li>');
+  }
+})
